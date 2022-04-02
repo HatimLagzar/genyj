c12c748f9c18d50a9ce12ed163cfda5271a66860
Hatim Lagzar <LHatimDev@gmail.com>
2022-04-02 11:42:44 +0000

    orders api endpoints

--- genyj-front/src/api/order/orderApi.js
+++ genyj-front/src/api/order/orderApi.js
@@ -1,5 +1,22 @@
 import axios from 'axios';
 
-export const createOrder = (formData) => {
-  return axios.post('/api/order', formData);
+export const createOrder = (formData, token = null) => {
+  let config = {
+    headers: {
+    }
+  }
+
+  if (token !== null) {
+    config.headers.Authorization = 'Bearer ' + token
+  }
+
+  return axios.post('/api/order', formData, config);
 };
+
+export const getOrder = (id) => {
+  return axios.get('/api/order/' + id);
+}
+
+export const saveAddress = (id, formData) => {
+  return axios.post('/api/order/' + id + '/address', formData);
+}
