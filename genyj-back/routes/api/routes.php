506bc18b5db0a36d8fde4cf727404744cbe98698
Hatim Lagzar <LHatimDev@gmail.com>
2022-04-02 13:22:18 +0000

    Update routes.php

--- genyj-back/routes/api/routes.php
+++ genyj-back/routes/api/routes.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\Auth\RefreshController;
 use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\Order\CreateOrderController;
 use App\Http\Controllers\Api\Order\GetOrderController;
+use App\Http\Controllers\Api\Order\GetOrderWithPaymentIntentController;
 use App\Http\Controllers\Api\Order\SaveAddressController;
 use App\Http\Controllers\Api\Products\GetFeaturedProductsController;
 use App\Http\Controllers\Api\Products\GetProductController;
@@ -39,6 +40,7 @@ Route::prefix('products')
 Route::prefix('order')
     ->group(function () {
         Route::get('{order}', GetOrderController::class);
+        Route::get('{order}/paymentIntent', GetOrderWithPaymentIntentController::class);
         Route::post('/', CreateOrderController::class);
         Route::post('{order}/address', SaveAddressController::class);
     });
