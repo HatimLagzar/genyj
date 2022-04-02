370c9176dbe8a22ce34aef152160c428dc1d03d0
Hatim Lagzar <LHatimDev@gmail.com>
2022-04-02 11:25:29 +0000

    Spinner Component

---
+++ genyj-front/src/components/Spinner/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Spinner.scss'
+
+export default function() {
+  return <div className="lds-ring mt-3 mx-auto">
+    <div/>
+    <div/>
+    <div/>
+    <div/>
+  </div>
+}
